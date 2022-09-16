166efaec89b8ab76060238b4793d97c430202333
Sunil Patil <sunilpatil10june@gmail.com>
2022-09-16 20:02:38 +0530

    initial setup for backend api

---
+++ api/src/main/java/com/cs/commandos/CommandosApplication.java
@@ -0,0 +1,13 @@
+package com.cs.commandos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandosApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/cs/commandos/CommandosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs.commandos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
