71f48ce4cc90bd344fe93e08e16fad75c5347e94
mangeshshinde <MAng12>
2022-09-16 22:03:53 +0530

    Initial Setup for Web App

--- web/src/components/LandingPage.js
+++ web/src/components/LandingPage.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import '../style/LandingPage.css';
 
-function App() {
+function LandingPage() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          All Components here
         </p>
         <a
           className="App-link"
@@ -15,11 +13,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Our Code Here
         </a>
       </header>
     </div>
   );
 }
 
-export default App;
+export default LandingPage;
